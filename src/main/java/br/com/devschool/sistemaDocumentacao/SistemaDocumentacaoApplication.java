6e589c814dec746f58d108ebd3b644cc5afe6bfd
Brunno <brunnosantana98@gmail.com>
2021-08-02 23:51:14 -0300

    primeiro commit

---
+++ src/main/java/br/com/devschool/sistemaDocumentacao/SistemaDocumentacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.devschool.sistemaDocumentacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDocumentacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDocumentacaoApplication.class, args);
+	}
+
+}
