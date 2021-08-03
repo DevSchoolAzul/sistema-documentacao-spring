ef4af644aa7aa93c0d00bc94c28007b647a97954
idmcarvalho <40929477+idmcarvalho@users.noreply.github.com>
2021-08-03 16:23:15 -0300

    Implementando as classes model Evento, PropriedadeRequisicao, Requisicao e TipoEvento

--- src/main/java/br/com/devschool/sistemaDocumentacao/domain/model/PropriedadeRequisicao.java
+++ src/main/java/br/com/devschool/sistemaDocumentacao/domain/model/PropriedadeRequisicao.java
@@ -1,4 +1,49 @@
 package br.com.devschool.sistemaDocumentacao.domain.model;
 
 public class PropriedadeRequisicao {
+    private Integer idPropriedade;
+    private Integer idRequisicao;
+    private String chave;
+    private String valor;
+    private Integer ordem;
+
+    public Integer getIdPropriedade() {
+        return idPropriedade;
+    }
+
+    public void setIdPropriedade(Integer idPropriedade) {
+        this.idPropriedade = idPropriedade;
+    }
+
+    public Integer getIdRequisicao() {
+        return idRequisicao;
+    }
+
+    public void setIdRequisicao(Integer idRequisicao) {
+        this.idRequisicao = idRequisicao;
+    }
+
+    public String getChave() {
+        return chave;
+    }
+
+    public void setChave(String chave) {
+        this.chave = chave;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    public Integer getOrdem() {
+        return ordem;
+    }
+
+    public void setOrdem(Integer ordem) {
+        this.ordem = ordem;
+    }
 }
