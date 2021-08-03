b4c59b0f4d69f36d19d7ad1f20f869cf3abf755a
Lucas Torquato <front.end.lb@gmail.com>
2021-08-03 15:49:05 -0300

    modelos projeto e versao atualizados

--- src/main/java/br/com/devschool/sistemaDocumentacao/domain/model/Versao.java
+++ src/main/java/br/com/devschool/sistemaDocumentacao/domain/model/Versao.java
@@ -1,4 +1,89 @@
 package br.com.devschool.sistemaDocumentacao.domain.model;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "versoes")
 public class Versao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String gmud;
+    private String descricao;
+    private LocalDate dataLancamento;
+    private Boolean situacao;
+    private Integer ordem;
+    private String numeroVersao;
+
+    @ManyToOne
+    private Projeto projeto;
+
+    public Versao() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getGmud() {
+        return gmud;
+    }
+
+    public void setGmud(String gmud) {
+        this.gmud = gmud;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDataLancamento() {
+        return dataLancamento;
+    }
+
+    public void setDataLancamento(LocalDate dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+
+    public Boolean getSituacao() {
+        return situacao;
+    }
+
+    public void setSituacao(Boolean situacao) {
+        this.situacao = situacao;
+    }
+
+    public Integer getOrdem() {
+        return ordem;
+    }
+
+    public void setOrdem(Integer ordem) {
+        this.ordem = ordem;
+    }
+
+    public String getNumeroVersao() {
+        return numeroVersao;
+    }
+
+    public void setNumeroVersao(String numeroVersao) {
+        this.numeroVersao = numeroVersao;
+    }
+
+    public Projeto getProjeto() {
+        return projeto;
+    }
+
+    public void setProjeto(Projeto projeto) {
+        this.projeto = projeto;
+    }
 }
