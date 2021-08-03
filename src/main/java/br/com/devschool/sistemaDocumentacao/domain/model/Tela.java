2a369629cc8266cf86bc3f8c9ff38fa90c788da6
Brunno <brunnosantana98@gmail.com>
2021-08-03 16:01:33 -0300

    Model Tela

--- src/main/java/br/com/devschool/sistemaDocumentacao/domain/model/Tela.java
+++ src/main/java/br/com/devschool/sistemaDocumentacao/domain/model/Tela.java
@@ -1,4 +1,125 @@
 package br.com.devschool.sistemaDocumentacao.domain.model;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
 public class Tela {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    private Versao versao;
+    private LocalDate dataCadastro;
+    private String nomeTela;
+    private String imagem;
+    private boolean situacao;
+    @ManyToOne
+    private Tela telaPai;
+    private int ordem;
+    private String urlLog;
+    @ManyToOne
+    private Versao versaoOrigem;
+    @OneToMany(mappedBy = "tela")
+    private List<Evento> eventos;
+    @OneToMany(mappedBy = "telaPai")
+    private List<Tela> telasFilhas;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Versao getVersao() {
+        return versao;
+    }
+
+    public void setVersao(Versao versao) {
+        this.versao = versao;
+    }
+
+    public LocalDate getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(LocalDate dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    public String getNomeTela() {
+        return nomeTela;
+    }
+
+    public void setNomeTela(String nomeTela) {
+        this.nomeTela = nomeTela;
+    }
+
+    public String getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(String imagem) {
+        this.imagem = imagem;
+    }
+
+    public boolean isSituacao() {
+        return situacao;
+    }
+
+    public void setSituacao(boolean situacao) {
+        this.situacao = situacao;
+    }
+
+    public Tela getTelaPai() {
+        return telaPai;
+    }
+
+    public void setTelaPai(Tela telaPai) {
+        this.telaPai = telaPai;
+    }
+
+    public int getOrdem() {
+        return ordem;
+    }
+
+    public void setOrdem(int ordem) {
+        this.ordem = ordem;
+    }
+
+    public String getUrlLog() {
+        return urlLog;
+    }
+
+    public void setUrlLog(String urlLog) {
+        this.urlLog = urlLog;
+    }
+
+    public Versao getVersaoOrigem() {
+        return versaoOrigem;
+    }
+
+    public void setVersaoOrigem(Versao versaoOrigem) {
+        this.versaoOrigem = versaoOrigem;
+    }
+
+    public List<Evento> getEventos() {
+        return eventos;
+    }
+
+    public void setEventos(List<Evento> eventos) {
+        this.eventos = eventos;
+    }
+
+    public List<Tela> getTelasFilhas() {
+        return telasFilhas;
+    }
+
+    public void setTelasFilhas(List<Tela> telasFilhas) {
+        this.telasFilhas = telasFilhas;
+    }
 }
