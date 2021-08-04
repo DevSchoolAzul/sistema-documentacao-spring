c97dd008fa6234eb5ae3da5b7977a934aab68a78
Lucas Torquato <front.end.lb@gmail.com>
2021-08-03 21:14:04 -0300

    adicionando alguns controllers

---
+++ src/main/java/br/com/devschool/sistemaDocumentacao/aplication/controller/ProjetoController.java
@@ -0,0 +1,10 @@
+package br.com.devschool.sistemaDocumentacao.aplication.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProjetoController {
+
+
+
+}
---
+++ src/main/java/br/com/devschool/sistemaDocumentacao/aplication/controller/VersaoController.java
@@ -0,0 +1,4 @@
+package br.com.devschool.sistemaDocumentacao.aplication.controller;
+
+public class VersaoController {
+}
