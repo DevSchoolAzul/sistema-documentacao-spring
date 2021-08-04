39ff46a7126631e0b4775b7226068be1b0119143
Lucas Torquato <front.end.lb@gmail.com>
2021-08-03 22:03:08 -0300

    alterações de estrutura do projeto

---
+++ src/main/java/br/com/devschool/sistemaDocumentacao/domain/external/EsxterbalExemple.java
@@ -0,0 +1,4 @@
+package br.com.devschool.sistemaDocumentacao.domain.external;
+
+public class EsxterbalExemple {
+}
---
+++ src/main/java/br/com/devschool/sistemaDocumentacao/domain/internal/dto/model/ProjetoDto.java
@@ -0,0 +1,4 @@
+package br.com.devschool.sistemaDocumentacao.domain.internal.dto.model;
+
+public class ProjetoDto {
+}
